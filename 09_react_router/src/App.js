c8d5f5e9133e2e8a10d64e6ce8562d6b23365dad
Prasad Kumar <kprasadkumar7@gmail.com>
2022-04-28 18:31:28 +0530

    used  useLocation useNavigate hooks

--- 09_react_router/src/App.js
+++ 09_react_router/src/App.js
@@ -1,26 +1,41 @@
-
-import './App.css';
-import Home from './Home';
-import NavBar from './NavBar';
-import Contact from './Contact';
-import About from './About';
-import User from './User';
-import NotFound from './NotFound';
-import {
-  Route,
-  Routes
-} from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import Contact from "./Contact";
+import About from "./About";
+import User from "./User";
+import NotFound from "./NotFound";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const a = "Prasad%20Kumar";
+  const b = a.replace("%20", " ");
+
+  console.log(b);
   return (
     <div className="App">
       <NavBar />
+      <hr />
+      
+      <span>
+        {location.pathname === "/"
+          ? "Home"
+          : location.pathname.includes("%20")
+          ? location.pathname.replace("%20", " ")
+          : location.pathname }
+      </span>
+      <br />
+      <button onClick={() => navigate(-1)}>Prev</button>
+      <button onClick={() => navigate(1)}>Next</button>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/about' element={ <About name='Prasad' />} />
-        <Route path='/user/:name/:age' element={ <User />} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About name="Prasad" />} />
+        <Route path="/user/:name/:age" element={<User />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
